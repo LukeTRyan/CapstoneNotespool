0ae49789679a2de49d50a1088c06f2f8a03fb67d
LukeTRyan <luke_t_ryan@hotmail.com>
2017-05-05 03:53:30 +1000

    updated database to postgres

--- capstonenotespool/capstonenotespool/settings.py
+++ capstonenotespool/capstonenotespool/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'capstonenotespool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'capstonenotespool',
+		'USER': 'postgres',
+		'PASSWORD': 'root',
+		'HOST': '',
+		'PORT': '',
     }
 }
 
