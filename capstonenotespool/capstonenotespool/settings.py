133578635f367ac4ce5001d604b485adeaba2b0a
LukeTRyan <luke_t_ryan@hotmail.com>
2017-05-04 22:44:20 +1000

    changed db to sqlite3 (mysql issues)

--- capstonenotespool/capstonenotespool/settings.py
+++ capstonenotespool/capstonenotespool/settings.py
@@ -75,12 +75,7 @@ WSGI_APPLICATION = 'capstonenotespool.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'capstonenotespool',
-        'USER': 'root',
-        'PASSWORD': 'Capstonenotespool',
-        'HOST': 'localhost',
-        'PORT': '3306',
+        'ENGINE': 'django.db.backends.sqlite3',
     }
 }
 
